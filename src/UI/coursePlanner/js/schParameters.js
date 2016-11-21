4440dad462e186fcaecbb058192146b0c7f27c91
Luke <zmx94824@gmail.com>
2016-11-21 15:18:33 -0800

    typo

--- src/UI/coursePlanner/js/schParameters.js
+++ src/UI/coursePlanner/js/schParameters.js
@@ -6,6 +6,6 @@ var db_key_id = "ID";
 var db_key_uid = "userID";
 var db_key_location = "Location";
 var db_key_title = "Title";
-var db_key_info = "Infor";
+var db_key_info = "Info";
 var db_key_color_bg = "BColour";
 var db_key_color_text = "TColour";
