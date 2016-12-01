911e78df6b29bb78b8283a6f73367a4d871d674a
Luke <zmx94824@gmail.com>
2016-12-01 15:33:24 -0800

    fix one bug

--- src/UI/coursePlanner/js/schDBParameters.js
+++ src/UI/coursePlanner/js/schDBParameters.js
@@ -1,9 +1,9 @@
 // Entry keys of the database
 var db_key_date = "Date";
-var db_key_start_date = "StartD";
-var db_key_end_date = "EndD";
-var db_key_start_time = "StartT";
-var db_key_end_time = "EndT";
+var db_key_start_date = "Start_Date";
+var db_key_end_date = "End_Date";
+var db_key_start_time = "Start";
+var db_key_end_time = "End";
 var db_key_id = "ID";
 var db_key_uid = "userID";
 var db_key_location = "Location";
